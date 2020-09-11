6c5e9fe6a6b425c7274943c16d9e41a86fe46b3b
unknown <Samuelcrudge@gmail.com>
2020-09-11 20:29:20 +0100

    Finished program

--- dndDice.py
+++ dndDice.py
@@ -8,14 +8,14 @@ class Dice:
         self.PlayersChoice = 0
         self.intro()
 
-    '''Intro to the application thats called on running Application'''
+    'Intro to the application thats called on running Application'
     def intro(self):
         print('Please Select a dice by the number of sides desired to roll.\nYour options are:\n2\n4\n6\n8\n12\n16\n18\n20\n100')
 
-    '''Takes user input and validates'''
+    'Takes user input and validates'
     def getPlayerOption(self):
         while self.PlayersChoice == 0:
-            self.PlayersChoice = int(input("Please select your number of sides"))
+            self.PlayersChoice = int(input("Please select your number of sides: \n"))
             if self.PlayersChoice not in [2, 4, 6, 8, 12, 16, 18, 20, 100]:
                 print("Please choose a valid choice.")
                 self.PlayersChoice = 0
@@ -28,4 +28,4 @@ class Dice:
 
 a = Dice()
 a.getPlayerOption()
-a.roll()
+print(a.roll())
