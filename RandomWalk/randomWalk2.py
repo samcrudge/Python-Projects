c1dd7034a97f9e029f50b12e670bd83c6c6dfa05
unknown <Samuelcrudge@gmail.com>
2020-09-22 14:24:17 +0100

    estimating walk % for walks that are under 4 from origin point

--- RandomWalk/randomWalk2.py
+++ RandomWalk/randomWalk2.py
@@ -7,7 +7,17 @@ def random_walk_2(n):
         x += dx
         y += dy
     return (x, y)
-for i in range(25):
-    walk = random_walk_2(10)
-    print(walk, "Distance from home = ",
-          abs(walk[0]) + abs(walk[1]))
+
+
+number_of_walks = 20000
+
+for walk_length in range(1, 31):
+    no_transport = 0
+    for i in range(number_of_walks):
+        (x, y) = random_walk_2(walk_length)
+        distance = abs(x) + abs(y)
+        if distance <=4:
+            no_transport += 1
+    no_transport_percentage = float(no_transport) / number_of_walks
+    print("walk size = ", walk_length,
+          "/ % of no transport = ", 100*no_transport_percentage)
